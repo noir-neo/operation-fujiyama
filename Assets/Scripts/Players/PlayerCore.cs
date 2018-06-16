b11a644dd996a1342ba0cfa3b4fa5f28c25414b7
Shoma SATO <noir.neo.04@gmail.com>
2018-06-16 18:35:55 +0900

    Shake

--- Assets/Scripts/Players/PlayerCore.cs
+++ Assets/Scripts/Players/PlayerCore.cs
@@ -2,6 +2,7 @@
 using UniRx;
 using UniRx.Triggers;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Players
 {
@@ -17,6 +18,9 @@ namespace Players
 
         public IReadOnlyReactiveProperty<Vector2> Velocity => _velocity;
         private readonly ReactiveProperty<Vector2> _velocity = new ReactiveProperty<Vector2>();
+
+        public IReadOnlyReactiveProperty<Vector2> Shake => _shake;
+        private readonly ReactiveProperty<Vector2> _shake = new ReactiveProperty<Vector2>();
 
         public IReadOnlyReactiveProperty<float> Torque => _torque;
         private readonly ReactiveProperty<float> _torque = new ReactiveProperty<float>();
@@ -48,6 +52,12 @@ namespace Players
                 .Select(v => v * Time.deltaTime)
                 .Subscribe(v => _altitude.Value -= v)
                 .AddTo(this);
+
+            this.UpdateAsObservable()
+                .Select(_ => RandomShake())
+                .WithLatestFrom(_altitude, CalcVelocity)
+                .Subscribe(x => _shake.Value = x)
+                .AddTo(this);
         }
 
         private Vector2 CalcVelocity(Vector2 v, float altitude)
@@ -59,6 +69,11 @@ namespace Players
         {
             _fallSpeed.Value += _param.FallSpeedAcceleration * Time.deltaTime;
             _torque.Value += _param.TorqueAcceleration * Time.deltaTime;
+        }
+
+        private Vector2 RandomShake()
+        {
+            return Random.insideUnitCircle * _param.ShakePower;
         }
 
         public IObservable<Vector2> Impact()
