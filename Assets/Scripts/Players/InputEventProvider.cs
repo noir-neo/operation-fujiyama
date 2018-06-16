af117b807f2b8cddbe3db64adbd271be62ef6382
Shoma SATO <noir.neo.04@gmail.com>
2018-06-16 14:13:55 +0900

    Move player

---
+++ Assets/Scripts/Players/InputEventProvider.cs
@@ -0,0 +1,25 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Players
+{
+    public class InputEventProvider : MonoBehaviour
+    {
+        public IReadOnlyReactiveProperty<Vector2> Move => _move;
+        private readonly ReactiveProperty<Vector2> _move = new ReactiveProperty<Vector2>();
+
+        void Start()
+        {
+            this.UpdateAsObservable()
+                .Select(_ => GetAxises().normalized)
+                .Subscribe(x => _move.Value = x)
+                .AddTo(this);
+        }
+
+        private Vector2 GetAxises()
+        {
+            return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        }
+    }
+}
