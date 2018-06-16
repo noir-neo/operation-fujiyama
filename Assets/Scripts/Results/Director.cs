497286ea62d666596af72973293d886312f1fdb6
Shota HAMADA <maku693@outlook.com>
2018-06-16 17:33:07 +0900

    Make explosion occurs only when the player was hit on the kaiju

--- Assets/Scripts/Results/Director.cs
+++ Assets/Scripts/Results/Director.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using Players;
 
 namespace Results
@@ -8,11 +9,18 @@ namespace Results
         [SerializeField]
         private GameObject _player;
 
+        [SerializeField]
+        private List<GameObject> _explosions;
+
         private void Awake()
         {
             Vector3 position = _player.transform.position;
             position.x = Store.Distance;
             _player.transform.position = position;
+
+            foreach (var explosion in _explosions) {
+                explosion.gameObject.SetActive(Store.IsHit);
+            }
         }
     }
 }
