2e77d8cad4b16ca283c02106b265fb2fb1957b29
Shota HAMADA <maku693@outlook.com>
2018-06-16 17:10:34 +0900

    Restart the game on pressing arrow keys on the result screen

---
+++ Assets/Scripts/Results/Restarter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restarter : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0.0f ||
+            Input.GetAxisRaw("Vertical") != 0.0f) {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
