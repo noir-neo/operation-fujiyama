81afa0dbec0dfd3f11d79718ea5821893d28b598
Shota HAMADA <maku693@outlook.com>
2018-06-16 19:00:04 +0900

    Remove `Result` namespace

---
+++ Assets/Scripts/Results/ResultLoader.cs
@@ -0,0 +1,28 @@
+﻿using Players;
+using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Results {
+    public class ResultLoader : MonoBehaviour
+    {
+        [SerializeField] private PlayerCore _playerCore;
+        [SerializeField] private float _hittableDistance;
+
+        void Start()
+        {
+            _playerCore.Impact()
+                .Subscribe(StoreAndLoadResult)
+                .AddTo(this);
+        }
+
+        private void StoreAndLoadResult(Vector2 v)
+        {
+            var distance = v.magnitude;
+            Store.Distance = distance;
+            Store.IsHit = distance < _hittableDistance;
+
+            SceneManager.LoadScene("Result");
+        }
+    }
+}
