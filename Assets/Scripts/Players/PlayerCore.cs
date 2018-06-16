02516f78c8c5d925e269a35a90bdafae740d6a9d
Shoma SATO <noir.neo.04@gmail.com>
2018-06-16 15:02:31 +0900

    Fall

--- Assets/Scripts/Players/PlayerCore.cs
+++ Assets/Scripts/Players/PlayerCore.cs
@@ -1,4 +1,5 @@
 ﻿using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 namespace Players
@@ -11,14 +12,34 @@ namespace Players
         [SerializeField]
         private InputEventProvider _input;
 
+        private static readonly float StartAltitude = 5f;
+
         public IReadOnlyReactiveProperty<Vector2> Velocity => _velocity;
         private readonly ReactiveProperty<Vector2> _velocity = new ReactiveProperty<Vector2>();
+
+        private readonly ReactiveProperty<float> _fallSpeed = new ReactiveProperty<float>();
+
+        public IReadOnlyReactiveProperty<float> Altitude => _altitude;
+        private readonly ReactiveProperty<float> _altitude = new ReactiveProperty<float>(StartAltitude);
 
         void Start()
         {
             _input.Move
                 .Select(CalcVelocity)
                 .Subscribe(v => _velocity.Value = v)
+                .AddTo(this);
+
+            _input.Move
+                .Skip(1)
+                .First()
+                // TODO: 徐々に加速する
+                .Subscribe(_ => _fallSpeed.Value = _param.Gravity)
+                .AddTo(this);
+
+            this.UpdateAsObservable()
+                .WithLatestFrom(_fallSpeed, (_, v) => v)
+                .Select(v => v * Time.deltaTime)
+                .Subscribe(v => _altitude.Value -= v)
                 .AddTo(this);
         }
 
